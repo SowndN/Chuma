3cc4f859bee2abcf8e25e9cf146e0d5f57571ecc
SowndN <sowndharyavb2696@gmail.com>
2022-03-10 12:09:01 +0530

    123456789

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,36 @@
+package org.add;
+
+public class GreensTech {
+         private void greensOmr() {
+		System.out.println("Greens OMR");
+
+		}
+         private void greensAdayar() {
+			System.out.println("Greens Adayar");
+
+		}
+         
+         private void greensTambaram() {
+			System.out.println("Greens Tambaram");
+		}
+         private void greensVelacherry() {
+			System.out.println("Greens Velacherry");
+		
+
+		}
+         private void greensAnnaNagar() {
+			System.out.println("Greens AnnaNagar");
+
+		}
+         
+         public static void main(String[] args) {
+			
+        	 GreensTech gt = new GreensTech();
+        	 gt.greensOmr();
+        	 gt.greensAdayar();
+        	 gt.greensTambaram();
+        	 gt.greensVelacherry();
+        	 gt.greensAnnaNagar();
+        	 
+		}
+}
